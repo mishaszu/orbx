ca5d4ae4905c65878e901803433cf791b7a279ff
Michal Szulczewski <mishaszu@gmail.com>
2019-04-26 12:58:44 +0200

    ORB-21 Updated linker spawn (#1)

--- src/core.orb.test.js
+++ src/core.orb.test.js
@@ -2,14 +2,13 @@ const { Tee } = require('node-tee');
 const link = require('./core.orb');
 
 const myTee = Tee.new('Core component');
+
 myTee.case('Should establish simple link between object orbs', expect => {
   const linker = link.spawn();
   const str1 = 'test1';
   const str2 = 'test2';
-  console.time('test1');
   const orb1 = linker.orb({ a: str1 });
   const orb2 = orb1.orb({ b: str2 });
-  console.timeEnd('test1');
   expect(orb2.b).toBe(str2);
   expect(orb2.a).toBe(str1);
 });
@@ -37,9 +36,29 @@ myTee.case('Should create clean linkers', expect => {
 });
 
 myTee.case('Should access itself properties', expect => {
-  const a = function Test() {console.log(this.a)}
+  const linker1 = link.spawn();
+  const a = function Test() {
+    return this.a;
+  };
   a.a = 2;
-  console.log(a.bind(a))
-})
+  const orb1 = linker1.orb(a);
+  expect(orb1.name).toBe('Test');
+  expect(orb1.render()).toBe(2);
+});
+
+myTee.case('Should create correct amount of childs', expect => {
+  const linker1 = link.spawn();
+  const orb1 = linker1.orb(() => {});
+  const orb2 = linker1.orb(() => {});
+  const orb3 = orb1.orb(() => {});
+  const orb4 = orb1.orb(() => {});
+  const linker2 = link.spawn();
+  expect(linker1.orbs.length).toBe(2);
+  expect(orb1.orbs.length).toBe(2);
+  expect(orb2.orbs.length).toBe(0);
+  expect(orb3.orbs.length).toBe(0);
+  expect(orb4.orbs.length).toBe(0);
+  expect(linker2.orbs.length).toBe(0);
+});
 
 module.exports = myTee;
